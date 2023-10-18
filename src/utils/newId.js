5f96905cae34be4a51677651c6d8551ce8acddf0
DiogoRDuarte <diogorduarte00@gmail.com>
2023-10-18 16:44:23 +0100

    Add license at the top of newId.js

--- src/utils/newId.js
+++ src/utils/newId.js
@@ -1,3 +1,10 @@
+/**
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, version 3 of the License.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * Other licensing options may be available, please reach out to data-viz@feedzai.com for more information.
+ */
+
 let lastId = 0;
 
 export default function (prefix = "id") {
