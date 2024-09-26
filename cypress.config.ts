527f30767c4348682612066086bd83c0a8fb23d3
Diogo Duarte <diogo.duarte@feedzai.com>
2024-09-26 12:35:15 +0100

    Enabled code coverage in the tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "cypress";
+import codeCoverageTask from "@cypress/code-coverage/task";
 
 export default defineConfig({
 	e2e: {
 		baseUrl: "http://localhost:5173/AutoVizuA11y/",
 		experimentalRunAllSpecs: true,
 		setupNodeEvents(on, config) {
-			// implement node event listeners here
+			codeCoverageTask(on, config);
+			return config;
 		},
 	},
 });
