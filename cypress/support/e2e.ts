6452022998eea5646dc16098a15006fc2dd6a56a
Diogo Duarte <diogo.duarte@feedzai.com>
2025-06-04 11:42:46 +0100

    Tried to fix github pipeline stopping at the tests step

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -17,6 +17,7 @@
 import "./commands";
 import "cypress-axe";
 import "cypress-plugin-tab";
+import "@cypress/code-coverage/support";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
